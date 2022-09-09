561631e1340ae1149f8d5f3ecadb2680531e5006
Oscar Davalos <44180633+odavalos@users.noreply.github.com>
2022-09-09 11:24:24 -0700

    Create utils.py

---
+++ trvae/utils.py
@@ -0,0 +1,260 @@
+from random import shuffle
+
+import numpy as np
+import scanpy as sc
+import torch
+from scipy import sparse
+from sklearn.preprocessing import LabelEncoder
+
+
+def normalize(adata, filter_min_counts=True, size_factors=True, normalize_input=True, logtrans_input=True,
+              n_top_genes=2000):
+    if filter_min_counts:
+        sc.pp.filter_genes(adata, min_counts=1)
+        sc.pp.filter_cells(adata, min_counts=1)
+
+    adata_count = adata.copy()
+
+    if size_factors:
+        sc.pp.normalize_per_cell(adata)
+        adata.obs['size_factors'] = adata.obs.n_counts / np.median(adata.obs.n_counts)
+    else:
+        adata.obs['size_factors'] = 1.0
+
+    if logtrans_input:
+        sc.pp.log1p(adata)
+
+    if n_top_genes > 0 and adata.shape[1] > n_top_genes:
+        sc.pp.highly_variable_genes(adata, n_top_genes=n_top_genes)
+        genes = adata.var['highly_variable']
+        adata = adata[:, genes]
+        adata_count = adata_count[:, genes]
+
+    if normalize_input:
+        sc.pp.scale(adata)
+
+    if sparse.issparse(adata_count.X):
+        adata_count.X = adata_count.X.A
+
+    if sparse.issparse(adata.X):
+        adata.X = adata.X.A
+
+    if size_factors or normalize_input or logtrans_input:
+        adata.raw = adata_count.copy()
+    else:
+        adata.raw = adata_count
+
+    return adata
+
+
+def train_test_split(adata, train_frac=0.85):
+    train_size = int(adata.shape[0] * train_frac)
+    indices = np.arange(adata.shape[0])
+    np.random.shuffle(indices)
+    train_idx = indices[:train_size]
+    test_idx = indices[train_size:]
+
+    train_data = adata[train_idx, :]
+    valid_data = adata[test_idx, :]
+
+    return train_data, valid_data
+
+
+def label_encoder(adata, label_encoder=None, condition_key='condition'):
+    """
+        Encode labels of Annotated `adata` matrix using sklearn.preprocessing.LabelEncoder class.
+        Parameters
+        ----------
+        adata: `~anndata.AnnData`
+            Annotated data matrix.
+        Returns
+        -------
+        labels: numpy nd-array
+            Array of encoded labels
+        Example
+        --------
+        >>> import scanpy as sc
+        >>> train_data = sc.read("./data/train.h5ad")
+        >>> train_labels, label_encoder = label_encoder(train_data)
+    """
+    if label_encoder is None:
+        le = LabelEncoder()
+        labels = le.fit_transform(adata.obs[condition_key].tolist())
+    else:
+        le = label_encoder
+        labels = np.zeros(adata.shape[0])
+        for condition, label in label_encoder.items():
+            labels[adata.obs[condition_key] == condition] = label
+    return labels.reshape(-1, 1), le
+
+
+def one_hot_encoder(idx, n_cls):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    assert torch.max(idx).item() < n_cls
+    if idx.dim() == 1:
+        idx = idx.unsqueeze(1)
+    onehot = torch.zeros(idx.size(0), n_cls)
+    onehot = onehot.to(device)
+    onehot.scatter_(1, idx.long(), 1)
+    return onehot
+
+
+def remove_sparsity(adata):
+    if sparse.issparse(adata.X):
+        adata = sc.AnnData(adata.X.todense(), obs=adata.obs, varm=adata.varm,
+                           var=adata.var)
+    return adata
+
+
+def create_dictionary(conditions, target_conditions=[]):
+    if isinstance(target_conditions, list):
+        target_conditions = [target_conditions]
+
+    dictionary = {}
+    conditions = [e for e in conditions if e not in target_conditions]
+    for idx, condition in enumerate(conditions):
+        dictionary[condition] = idx
+    return dictionary
+
+
+def shuffle_adata(adata):
+    """
+        Shuffles the `adata`.
+        # Parameters
+        adata: `~anndata.AnnData`
+            Annotated data matrix.
+        labels: numpy nd-array
+            list of encoded labels
+        # Returns
+            adata: `~anndata.AnnData`
+                Shuffled annotated data matrix.
+            labels: numpy nd-array
+                Array of shuffled labels if `labels` is not None.
+        # Example
+        ```python
+        import scgen
+        import anndata
+        import pandas as pd
+        train_data = anndata.read("./data/train.h5ad")
+        train_labels = pd.read_csv("./data/train_labels.csv", header=None)
+        train_data, train_labels = shuffle_data(train_data, train_labels)
+        ```
+    """
+    adata = remove_sparsity(adata)
+
+    ind_list = [i for i in range(adata.shape[0])]
+    shuffle(ind_list)
+    new_adata = adata[ind_list, :]
+    return new_adata
+
+
+def partition(data, partitions, num_partitions):
+    res = []
+    partitions = partitions.flatten()
+    for i in range(num_partitions):
+        res += [data[(partitions == i).nonzero().squeeze(1)]]
+    return res
+
+
+#all credit for the code in this module goes to https://github.com/josipd/torch-two-sample
+def pdist(sample_1, sample_2, norm=2, eps=1e-5):
+    r"""Compute the matrix of all squared pairwise distances.
+    Arguments
+    ---------
+    sample_1 : torch.Tensor or Variable
+        The first sample, should be of shape ``(n_1, d)``.
+    sample_2 : torch.Tensor or Variable
+        The second sample, should be of shape ``(n_2, d)``.
+    norm : float
+        The l_p norm to be used.
+    Returns
+    -------
+    torch.Tensor or Variable
+        Matrix of shape (n_1, n_2). The [i, j]-th entry is equal to
+        ``|| sample_1[i, :] - sample_2[j, :] ||_p``."""
+    n_1, n_2 = sample_1.size(0), sample_2.size(0)
+    norm = float(norm)
+    if norm == 2.:
+        norms_1 = torch.sum(sample_1**2, dim=1, keepdim=True)
+        norms_2 = torch.sum(sample_2**2, dim=1, keepdim=True)
+        norms = (norms_1.expand(n_1, n_2) +
+                 norms_2.transpose(0, 1).expand(n_1, n_2))
+        distances_squared = norms - 2 * sample_1.mm(sample_2.t())
+        return torch.sqrt(eps + torch.abs(distances_squared))
+    else:
+        dim = sample_1.size(1)
+        expanded_1 = sample_1.unsqueeze(1).expand(n_1, n_2, dim)
+        expanded_2 = sample_2.unsqueeze(0).expand(n_1, n_2, dim)
+        differences = torch.abs(expanded_1 - expanded_2) ** norm
+        inner = torch.sum(differences, dim=2, keepdim=False)
+        return (eps + inner) ** (1. / norm)
+
+
+class MMDStatistic:
+    r"""The *unbiased* MMD test of :cite:`gretton2012kernel`.
+    The kernel used is equal to:
+    .. math ::
+        k(x, x') = \sum_{j=1}^k e^{-\alpha_j\|x - x'\|^2},
+    for the :math:`\alpha_j` proved in :py:meth:`~.MMDStatistic.__call__`.
+    Arguments
+    ---------
+    n_1: int
+        The number of points in the first sample.
+    n_2: int
+        The number of points in the second sample."""
+
+    def __init__(self, n_1, n_2):
+        self.n_1 = n_1
+        self.n_2 = n_2
+
+        # The three constants used in the test.
+        self.a00 = 1. / (n_1 * (n_1 - 1))
+        self.a11 = 1. / (n_2 * (n_2 - 1))
+        self.a01 = - 1. / (n_1 * n_2)
+
+    def __call__(self, sample_1, sample_2, alphas, ret_matrix=False):
+        r"""Evaluate the statistic.
+        The kernel used is
+        .. math::
+            k(x, x') = \sum_{j=1}^k e^{-\alpha_j \|x - x'\|^2},
+        for the provided ``alphas``.
+        Arguments
+        ---------
+        sample_1: :class:`torch:torch.autograd.Variable`
+            The first sample, of size ``(n_1, d)``.
+        sample_2: variable of shape (n_2, d)
+            The second sample, of size ``(n_2, d)``.
+        alphas : list of :class:`float`
+            The kernel parameters.
+        ret_matrix: bool
+            If set, the call with also return a second variable.
+            This variable can be then used to compute a p-value using
+            :py:meth:`~.MMDStatistic.pval`.
+        Returns
+        -------
+        :class:`float`
+            The test statistic.
+        :class:`torch:torch.autograd.Variable`
+            Returned only if ``ret_matrix`` was set to true."""
+        sample_12 = torch.cat((sample_1, sample_2), 0)
+        distances = pdist(sample_12, sample_12, norm=2)
+
+        kernels = None
+        for alpha in alphas:
+            kernels_a = torch.exp(- alpha * distances ** 2)
+            if kernels is None:
+                kernels = kernels_a
+            else:
+                kernels = kernels + kernels_a
+
+        k_1 = kernels[:self.n_1, :self.n_1]
+        k_2 = kernels[self.n_1:, self.n_1:]
+        k_12 = kernels[:self.n_1, self.n_1:]
+
+        mmd = (2 * self.a01 * k_12.sum() +
+               self.a00 * (k_1.sum() - torch.trace(k_1)) +
+               self.a11 * (k_2.sum() - torch.trace(k_2)))
+        if ret_matrix:
+            return mmd, kernels
+        else:
+            return mmd
